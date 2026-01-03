fe47edb85964e0d73ced95e7849d9af4c40cd061
coolutkarshraj <utkarshcpr@gmail.com>
2026-01-04 01:48:20 +0530

    Vhal Demo full working App

---
+++ app/src/main/java/com/io/utkarsh/vhal_demo_app/interfaces/IBasePropertyHandler.java
@@ -0,0 +1,9 @@
+package com.io.utkarsh.vhal_demo_app.interfaces;
+
+public interface IBasePropertyHandler {
+    void getDefaultProperties();
+
+    void registerProperties();
+
+    void unRegisterProperties();
+}
